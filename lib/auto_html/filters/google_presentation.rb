4bde60c0a597ef9f310c8ed13ebb673c199887b0
Mark Miyashita <negativetwelve@gmail.com>
2014-10-18 10:27:57 -0700

    Set the source to be just the entire text

--- lib/auto_html/filters/google_presentation.rb
+++ lib/auto_html/filters/google_presentation.rb
@@ -2,6 +2,8 @@
 AutoHtml.add_filter(:google_presentation).with(width: 420, height: 315, style: "color: #000; text-align: left") do |text, options|
   regex = /(https?):\/\/docs\.google\.([a-z\.]+)\/presentation\?(.*)/
   text.gsub(regex) do
+    # For now, we're expecting that the text is only the link to the Google Presentation
+    src = text
     %{<iframe src="#{src}" frameborder="0" width="960" height="749" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>}
   end
 end

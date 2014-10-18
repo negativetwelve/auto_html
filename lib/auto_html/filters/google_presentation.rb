3bc99f1e68c68ba8b7622ca1d86dc2a96d8a3eef
Mark Miyashita <negativetwelve@gmail.com>
2014-10-18 10:52:33 -0700

    Use the src link instead of text

--- lib/auto_html/filters/google_presentation.rb
+++ lib/auto_html/filters/google_presentation.rb
@@ -5,5 +5,5 @@ AutoHtml.add_filter(:google_presentation).with(width: 420, height: 315, style: "
   src = text
   src.gsub("/pub?", "/embed?")
   # For now, we're expecting that the text is only the link to the Google Presentation
-  %{<iframe src="#{text}" frameborder="0" width="960" height="749" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>}
+  %{<iframe src="#{src}" frameborder="0" width="960" height="749" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>}
 end

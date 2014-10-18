e89ce2a2d20f31f70af3e231ec02ae783ce1fe64
Mark Miyashita <negativetwelve@gmail.com>
2014-10-18 11:00:46 -0700

    Allow the width and height to be adjusted

--- lib/auto_html/filters/google_presentation.rb
+++ lib/auto_html/filters/google_presentation.rb
@@ -4,6 +4,9 @@ AutoHtml.add_filter(:google_presentation).with(width: 420, height: 315, style: "
 
   src = text
   src = src.gsub("/pub?", "/embed?")
+  width = options[:width]
+  height = options[:height]
+
   # For now, we're expecting that the text is only the link to the Google Presentation
-  %{<iframe src="#{src}" frameborder="0" width="960" height="749" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>}
+  %{<iframe src="#{src}" frameborder="0" width="#{width}" height="#{height}" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>}
 end

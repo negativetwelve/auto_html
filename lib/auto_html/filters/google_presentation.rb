d51ef33765a611ef6d7f07dfa6f1a0ab25d007ce
Mark Miyashita <negativetwelve@gmail.com>
2014-10-18 10:39:22 -0700

    Just use the text as the supposed google presentation link

--- lib/auto_html/filters/google_presentation.rb
+++ lib/auto_html/filters/google_presentation.rb
@@ -1,9 +1,7 @@
 # encoding: UTF-8
 AutoHtml.add_filter(:google_presentation).with(width: 420, height: 315, style: "color: #000; text-align: left") do |text, options|
   regex = /(https?):\/\/docs\.google\.([a-z\.]+)\/presentation\?(.*)/
-  text.gsub(regex) do
-    # For now, we're expecting that the text is only the link to the Google Presentation
-    src = text
-    %{<iframe src="#{src}" frameborder="0" width="960" height="749" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>}
-  end
+
+  # For now, we're expecting that the text is only the link to the Google Presentation
+  %{<iframe src="#{text}" frameborder="0" width="960" height="749" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>}
 end

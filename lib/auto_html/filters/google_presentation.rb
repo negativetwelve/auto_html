4cd3a699209f9438e79bcf223252b284a355b00a
Mark Miyashita <negativetwelve@gmail.com>
2014-10-18 10:49:52 -0700

    Replace pub with embed for google presentatins

--- lib/auto_html/filters/google_presentation.rb
+++ lib/auto_html/filters/google_presentation.rb
@@ -2,6 +2,8 @@
 AutoHtml.add_filter(:google_presentation).with(width: 420, height: 315, style: "color: #000; text-align: left") do |text, options|
   regex = /(https?):\/\/docs\.google\.([a-z\.]+)\/presentation\?(.*)/
 
+  src = text
+  src.gsub("/pub?", "/embed?")
   # For now, we're expecting that the text is only the link to the Google Presentation
   %{<iframe src="#{text}" frameborder="0" width="960" height="749" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>}
 end
